61d0ee90d8e69704c74873414dd75c6b4581478d
pewish <marinasavoskin@gamil.com>
2024-05-17 19:03:47 +0300

    Kotlin

--- app/src/main/java/com/example/text_app/Constants.kt
+++ app/src/main/java/com/example/text_app/Constants.kt
@@ -2,5 +2,6 @@ package com.example.mulriplepermissionhandleapp.com.example.text_app
 
 object Constants {
     const val TAG = "cameraX"
+    const val CODE = 100
     const val FILE_NAME_FORMAT = "yy-MM-dd-HH-ss-SSS"
 }
